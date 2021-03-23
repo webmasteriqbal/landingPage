1ece50204f8b9277754026a7f5d804d069a8c389
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-03-23 16:23:56 +0600

    seo tool added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
 <html class="no-js" lang="zxx">
 
 <head>
+    <!-- meta Tag -->
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Welcome | Keus</title>
-    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    {!! SEO::generate() !!}
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
